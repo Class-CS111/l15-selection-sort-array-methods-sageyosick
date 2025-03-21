d4bf675e8fa55032ace9e695e9d74ec8d19cc8c7
sageyosick <sageyosick@gmail.com>
2025-03-21 20:30:31 +0000

    selection sort array methods

--- Main.java
+++ Main.java
@@ -9,6 +9,31 @@ public class Main
 {
   public static void main(String[] args)
   {
+    int[] values = {12, 16, 26, 42, 53, 77, 84};
+
+    System.out.println("Test :" + ArrayMethods.arrayString(values));
+
+    ArrayMethods.swap(values, 0, 6);
+    System.out.println("Test after swap (12and84):" + ArrayMethods.arrayString(values));
+
+    int[] numbers = {42, 16, 84, 12, 77, 26, 53};
+    int minIndex1 = ArrayMethods.indexOfMin(numbers, 0);
+    int minIndex2 = ArrayMethods.indexOfMin(numbers, 4);
+
+    System.out.println("TEST: min indices should be 3 and 5, we got: " + minIndex1 + " and " + minIndex2);
+
+    System.out.println("Test before reversal:" + ArrayMethods.arrayString(numbers));
+
+    ArrayMethods.reverse(numbers);
+
+    System.out.println("Test after reversal:" + ArrayMethods.arrayString(numbers));
+
+    System.out.println("Test before sorting:" + ArrayMethods.arrayString(numbers));
+
+    ArrayMethods.selectionSort(numbers);
+
+    System.out.println("Test after sorting:" + ArrayMethods.arrayString(numbers));
 
   }
 }
+
